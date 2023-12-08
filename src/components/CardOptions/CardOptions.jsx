85343cabe286e1e881d9670986bff673b1425de5
Nikolay Shein <qseboy1998@gmail.com>
2023-12-08 22:28:16 +0200

    add layout to CatalogPage

--- src/components/CardOptions/CardOptions.jsx
+++ src/components/CardOptions/CardOptions.jsx
@@ -1,5 +1,73 @@
-const CardOptions = () => {
-  <li></li>;
+import PropTypes from "prop-types";
+import formatAccessories from "../../helpers/formatAccessories";
+const CardOptions = ({
+  city,
+  country,
+  rentalCompany,
+  type,
+  make,
+  id,
+  accessories,
+  functionalities,
+}) => {
+  const shortedAccess = formatAccessories(accessories, functionalities);
+
+  return (
+    <ul className="flex flex-wrap mb-auto">
+      <li className="flex items-center">
+        <span className="text-neutral-900 text-opacity-50 text-xs leading-[18px]">
+          {city}
+        </span>
+        <span className="border-l h-full mx-[6px]"></span>
+      </li>
+      <li className="flex items-center">
+        <span className="text-neutral-900 text-opacity-50 text-xs leading-[18px]">
+          {country}
+        </span>
+        <span className="border-l h-full mx-[6px]"></span>
+      </li>
+      <li className="flex items-center">
+        <span className="text-neutral-900 text-opacity-50 text-xs leading-[18px]">
+          {rentalCompany}
+        </span>
+        <span className="border-l h-full mx-[6px]"></span>
+      </li>
+      <li className="flex items-center">
+        <span className="text-neutral-900 text-opacity-50 text-xs leading-[18px]">
+          {type}
+        </span>
+        <span className="border-l h-full mx-[6px]"></span>
+      </li>
+      <li className="flex items-center">
+        <span className="text-neutral-900 text-opacity-50 text-xs leading-[18px]">
+          {make}
+        </span>
+        <span className="border-l h-full mx-[6px]"></span>
+      </li>
+      <li className="flex items-center">
+        <span className="text-neutral-900 text-opacity-50 text-xs leading-[18px]">
+          {id}
+        </span>
+        <span className="border-l h-full mx-[6px]"></span>
+      </li>
+      <li className="flex items-center">
+        <span className="text-neutral-900 text-opacity-50 text-xs leading-[18px]">
+          {shortedAccess}
+        </span>
+      </li>
+    </ul>
+  );
+};
+
+CardOptions.propTypes = {
+  city: PropTypes.string,
+  country: PropTypes.string,
+  rentalCompany: PropTypes.string,
+  type: PropTypes.string,
+  make: PropTypes.string,
+  id: PropTypes.number,
+  accessories: PropTypes.array,
+  functionalities: PropTypes.array,
 };
 
 export default CardOptions;
--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,10 +1,13 @@
 import Card from "../components/Card/Card";
+import { useGetCarsQuery } from "../redux/cars/cars-slice";
+// import data from "../bd.json";
 
 const CatalogPage = () => {
+  const { data } = useGetCarsQuery();
   return (
     <>
-      <ul className="list-none flex flex-wrap">
-        <Card />
+      <ul className="list-none flex flex-wrap gap-x-[30px] gap-y-[50px]">
+        {data && data.map((el) => <Card key={el.id} data={el} />)}
       </ul>
     </>
   );
